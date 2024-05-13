22dbeb39aec01c6f84a6db8de83a1f8f44123288
MinhLuanLu <minhlu14206@gmail.com>
2024-05-13 10:42:14 +0200

    change Email in Database to unique

--- django_main/my_smartwork_app/models.py
+++ django_main/my_smartwork_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Employee(models.Model):
     FullName = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=50)
+    Email = models.EmailField(max_length=50, unique=True)
     Address = models.CharField(max_length=100)
     City = models.CharField(max_length=50)
     Postcode = models.CharField(max_length=8)
